565be30d8b8636710101d824e0adca3543ef3f3c
Bhuvan1326 <bhuvanpatil1313@gmail.com>
2026-09-11 12:26:23 +0530

    Deployment Issue Fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/backend/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/api/v1/:path*`,
+        destination: `${process.env.BACKEND_URL || "http://localhost:8000"}/api/v1/:path*`,      
       },
     ];
   },
